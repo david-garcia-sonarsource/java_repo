4ab82841ad83443b690f1f04e1f636875e965528
david-garcia-sonarsource <109538884+david-garcia-sonarsource@users.noreply.github.com>
2023-02-09 15:00:30 -0600

    Added someString

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 // Your First Program
 
 class HelloWorld {
+    
+    private String someString;
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
         System.out.println("This is only a test");
